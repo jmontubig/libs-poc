5e94e200149aaf879d71eacc762b9a64fd923dcc
Nathan <xtrainingph@gmail.com>
2020-02-18 12:24:30 +0800

    Fix issue for directive for export

--- projects/lib/src/lib/directives/lib-highlight.directive.ts
+++ projects/lib/src/lib/directives/lib-highlight.directive.ts
@@ -1,14 +1,17 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import { Directive, ElementRef, Input, HostListener, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[libHighlight]'
 })
-export class LibHighlightDirective {
+export class LibHighlightDirective implements OnInit {
+ 
 
   @Input('libHighlight') highlightColor: string;
   
-  constructor(private el: ElementRef) { 
-    el.nativeElement.style.backgroundColor = this.highlightColor;
-  }
+  constructor(private el: ElementRef) { }
 
+  ngOnInit(): void {
+    console.log(this.highlightColor);
+    this.el.nativeElement.style.backgroundColor = this.highlightColor;
+  }
 }
